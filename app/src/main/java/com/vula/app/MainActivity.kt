02d22030c1de413792f422596139c4dfe639f869
Fredrick Lani <fredricklani@gmail.com>
2026-04-23 14:07:32 +0300

    feat: Lock server IP and clean up discovery screen

--- app/src/main/java/com/vula/app/MainActivity.kt
+++ app/src/main/java/com/vula/app/MainActivity.kt
@@ -10,13 +10,6 @@ import androidx.compose.ui.Modifier
 import com.vula.app.core.ui.theme.VulaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.vula.app.core.ui.ServerDiscoveryScreen
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,20 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppRoot()
+                    com.vula.app.navigation.VulaApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun AppRoot() {
-    var isServerFound by remember { mutableStateOf(false) }
-    
-    if (!isServerFound) {
-        ServerDiscoveryScreen(onServerFound = { isServerFound = true })
-    } else {
-        com.vula.app.navigation.VulaApp()
     }
 }
